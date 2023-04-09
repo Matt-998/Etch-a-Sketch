408b9e75d05c517c1edc1af50ccdbce45b9e56d4
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-04-09 16:17:59 +1000

    Try to implement grid size slider

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 let container = document.getElementById("container");
+let slider = document.getElementById("range");
+let sliderValue = document.getElementById("sliderValue");
+let reset = document.getElementById("reset");
 
-let input = 10;
-let output = input * input;
+sliderValue.textContent = slider.value;
+slider.oninput = function () {
+  sliderValue.textContent = this.value;
+  generateGrid(this.value);
+};
+let grid = slider.value;
 
-for (let i = 0; i < output; i++) {
-  if (i % input === 0) {
-    let lbreak = document.createElement("div");
-    lbreak.classList.add("break");
-    container.appendChild(lbreak);
+generateGrid(grid);
+// Not working
+reset.addEventListener("click", generateGrid(grid));
+
+function removeAllChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
-  let square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
 }
 
-let squares = document.querySelectorAll("div.square");
-
-for (const square of squares) {
-  square.addEventListener("mouseover", function () {
-    this.style.backgroundColor = getRandomColour();
-  });
+function generateGrid(grid) {
+  removeAllChildren(container);
+  let input = grid;
+  let output = input * input;
+  for (let i = 0; i < output; i++) {
+    if (i % input === 0) {
+      let lbreak = document.createElement("div");
+      lbreak.classList.add("break");
+      container.appendChild(lbreak);
+    }
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
+  // Not working
+  if (grid != "15") {
+    slider.addEventListener("mouseup", addColour);
+  } else {
+    addColour;
+  }
+}
+function addColour() {
+  let squares = document.querySelectorAll("div.square");
+  for (const square of squares) {
+    square.addEventListener("mouseover", function () {
+      this.style.backgroundColor = getRandomColour();
+    });
+  }
 }
 
 const getRandomNumber = (maxNum) => {
@@ -30,5 +58,5 @@ const getRandomColour = () => {
   const h = getRandomNumber(360);
   const s = getRandomNumber(100);
   const l = getRandomNumber(100);
-  return `hsl(${308}, ${s}%, ${l}%)`;
+  return `hsl(${200}, ${s}%, ${l}%)`;
 };
