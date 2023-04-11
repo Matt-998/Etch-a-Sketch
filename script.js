ccb4d361e376c1b0f81276482225c2dba6feea38
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-04-11 21:55:37 +1000

    Fix/ add reset button and colour selection buttons

--- script.js
+++ script.js
@@ -2,17 +2,43 @@ let container = document.getElementById("container");
 let slider = document.getElementById("range");
 let sliderValue = document.getElementById("sliderValue");
 let reset = document.getElementById("reset");
+let black = document.getElementById("black");
+let white = document.getElementById("white");
+let cold = document.getElementById("cold");
+let warm = document.getElementById("warm");
 
+let grid = slider.value;
 sliderValue.textContent = slider.value;
+let colourSelect = "random";
+
 slider.oninput = function () {
   sliderValue.textContent = this.value;
-  generateGrid(this.value);
+  grid = this.value;
+  generateGrid(grid);
 };
-let grid = slider.value;
 
+cold.addEventListener("click", function () {
+  colourSelect = "cold";
+  addColour();
+});
+warm.addEventListener("click", function () {
+  colourSelect = "warm";
+  addColour();
+});
+black.addEventListener("click", function () {
+  colourSelect = "black";
+  addColour();
+});
+white.addEventListener("click", function () {
+  colourSelect = "white";
+  addColour();
+});
+
+reset.addEventListener("click", function () {
+  generateGrid(grid);
+  addColour();
+});
 generateGrid(grid);
-// Not working
-reset.addEventListener("click", generateGrid(grid));
 
 function removeAllChildren(parent) {
   while (parent.firstChild) {
@@ -22,41 +48,59 @@ function removeAllChildren(parent) {
 
 function generateGrid(grid) {
   removeAllChildren(container);
-  let input = grid;
-  let output = input * input;
+  let output = grid * grid;
   for (let i = 0; i < output; i++) {
-    if (i % input === 0) {
-      let lbreak = document.createElement("div");
-      lbreak.classList.add("break");
-      container.appendChild(lbreak);
+    if (i % grid === 0 && i != 0) {
+      let lineBreak = document.createElement("div");
+      lineBreak.classList.add("break");
+      container.appendChild(lineBreak);
     }
     let square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
   }
-  // Not working
   if (grid != "15") {
-    slider.addEventListener("mouseup", addColour);
+    slider.addEventListener("mouseup", addColour());
   } else {
-    addColour;
+    addColour();
   }
 }
+
 function addColour() {
   let squares = document.querySelectorAll("div.square");
   for (const square of squares) {
     square.addEventListener("mouseover", function () {
-      this.style.backgroundColor = getRandomColour();
+      if (colourSelect === "cold") {
+        square.style.backgroundColor = getRandomColdColour();
+      } else if (colourSelect === "warm") {
+        square.style.backgroundColor = getRandomWarmColour();
+      } else if (colourSelect === "black") {
+        square.style.backgroundColor = "black";
+      } else {
+        square.style.backgroundColor = "white";
+      }
     });
   }
 }
 
-const getRandomNumber = (maxNum) => {
-  return Math.floor(Math.random() * maxNum);
-};
+// function getRandomNumber(maxNum) {
+//   return Math.floor(Math.random() * maxNum);
+// }
 
-const getRandomColour = () => {
-  const h = getRandomNumber(360);
-  const s = getRandomNumber(100);
-  const l = getRandomNumber(100);
-  return `hsl(${200}, ${s}%, ${l}%)`;
-};
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomColdColour() {
+  const h = getRandomNumber(200, 210);
+  const s = getRandomNumber(30, 100);
+  const l = getRandomNumber(30, 100);
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function getRandomWarmColour() {
+  const h = getRandomNumber(0, 5);
+  const s = getRandomNumber(30, 100);
+  const l = getRandomNumber(30, 100);
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
